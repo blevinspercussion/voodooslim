06ad78a6d0f70e8c432a921336791b608fe1dc03
Adam Blevins <blevinspercussion@gmail.com>
2023-09-17 14:00:03 -0500

    Fixed large video file issue

---
+++ script.js
@@ -0,0 +1,25 @@
+// Functionality for the About page
+
+let ianPic = document.getElementById("ian-pic");
+let jimPic = document.getElementById("jim-pic");
+let rodPic = document.getElementById("rod-pic");
+let adamPic = document.getElementById("adam-pic");
+
+let bioPics = [ianPic, jimPic, rodPic, adamPic];
+
+ianPic.addEventListener("click", (e) => {
+  for (let i = 0; i < bioPics.length; i++) {
+    if (bioPics[i] !== ianPic) {
+      bioPics[i].classList.remove("shown");
+      bioPics[i].classList.add("hidden");
+    }
+  }
+});
+
+jimPic.addEventListener("click", () => {});
+
+rodPic.addEventListener("click", () => {});
+
+adamPic.addEventListener("click", () => {
+  console.log("adam clicked");
+});
